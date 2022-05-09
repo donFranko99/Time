f1d5d034ea5d49a5b9082469fe5e9e7023ca9a0f
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2022-05-09 11:47:39 +0200

    Add project files.

---
+++ TimeAndTimePeriod/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAndTimePeriod
+{
+    public struct Time
+    {
+        public byte Hours { get; set; }
+        public byte Minutes { get; set; }
+        public byte Seconds { get; set; }
+    }
+}
