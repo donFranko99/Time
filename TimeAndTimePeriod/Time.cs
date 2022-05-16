5a6d1924e51bd8ac41f86c608db92faa4483011b
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2022-05-16 13:05:06 +0200

    implemented both interfaces iequatable and icomparable

--- TimeAndTimePeriod/Time.cs
+++ TimeAndTimePeriod/Time.cs
@@ -6,10 +6,86 @@ using System.Threading.Tasks;
 
 namespace TimeAndTimePeriod
 {
-    public struct Time
+    public struct Time : IEquatable<Time>, IComparable<Time>
     {
-        public byte Hours { get; set; }
-        public byte Minutes { get; set; }
-        public byte Seconds { get; set; }
+        public  byte Hours { get; }
+        public  byte Minutes { get; }
+        public  byte Seconds { get; }
+
+        public Time(byte hours, byte minutes=0, byte seconds=0)
+        {
+            Hours = (byte)(hours % 24);
+            Minutes = (byte)(minutes % 60);
+            Seconds = (byte)(seconds % 60);
+        }
+        public Time(string time)
+        {
+            var data = time.Split(':');
+            Hours = (byte)(Byte.Parse(data[0])%24);
+            Minutes = (byte)(Byte.Parse(data[1])%60);
+            Seconds = (byte)(Byte.Parse(data[2])%60);
+        }
+        public static Time Zero() => new Time(0);
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Time time &&
+                   Hours == time.Hours &&
+                   Minutes == time.Minutes &&
+                   Seconds == time.Seconds;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hours, Minutes, Seconds);
+        }
+
+        public bool Equals(Time other)
+        {
+            return (Hours==other.Hours && Minutes == other.Minutes && Seconds == other.Seconds);
+        }
+
+        public int CompareTo(Time other)
+        {
+            if (Hours-other.Hours != 0)
+            {
+                return Hours - other.Hours;
+            }
+            if (Minutes - other.Minutes != 0)
+            {
+                return Minutes - other.Minutes;
+            }
+            return Seconds - other.Seconds;
+        }
+
+        public static bool operator ==(Time left, Time right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Time left, Time right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(Time left, Time right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(Time left, Time right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(Time left, Time right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(Time left, Time right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
     }
 }
