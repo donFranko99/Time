3090a919c3b0a7a87731b4492da89bad515b4dd4
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2022-05-19 11:32:01 +0200

    Added TimePeriod structure, implemented interfaces equatable and comparable

---
+++ TimeAndTimePeriod/TimePeriod.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAndTimePeriod
+{
+    public struct TimePeriod : IEquatable<TimePeriod>, IComparable<TimePeriod>
+    {
+        private readonly long seconds;
+        public string Duration { get => $"{seconds/3600}:{(seconds%3600)/60}:{(seconds%3600)%60}"; }
+
+        public TimePeriod(long h, long m , long s = 0)
+        {
+            seconds = h*3600 + m*60 + s;
+        }
+        public TimePeriod(long s = 0)
+        {
+            seconds = s;
+        }
+        public TimePeriod(Time t1, Time t2)
+        {
+            seconds = Math.Abs((t2.Hours-t1.Hours)*3600 + (t2.Minutes-t1.Minutes)*60 + (t2.Seconds-t1.Seconds));
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TimePeriod period &&
+                   seconds == period.seconds;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(seconds);
+        }
+
+        public bool Equals(TimePeriod other)
+        {
+            if (seconds == other.seconds)
+                return true;
+            return false;
+        }
+
+        public int CompareTo(TimePeriod other)
+        {
+            return Math.Sign(seconds - other.seconds);
+        }
+
+        public static bool operator ==(TimePeriod left, TimePeriod right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TimePeriod left, TimePeriod right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(TimePeriod left, TimePeriod right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(TimePeriod left, TimePeriod right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(TimePeriod left, TimePeriod right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(TimePeriod left, TimePeriod right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+    }
+}
